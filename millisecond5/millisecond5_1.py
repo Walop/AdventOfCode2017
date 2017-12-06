5eb2afe5c76b4e9e12e32c3edb14683284fb99ec
Joni Lehmuksela <joni.lehmuksela@gmail.com>
2017-12-06 16:20:11 +0200

    Solutions for day 5

---
+++ millisecond5/millisecond5_1.py
@@ -0,0 +1,11 @@
+with open("input") as file:
+    input_list = list(map(lambda x: int(x), file.read().split("\n")))
+    index = 0
+    jumps = 0
+    while index > -1 and index < len(input_list):
+        jumps += 1
+        next_index = index + input_list[index]
+        input_list[index] += 1
+        index = next_index
+    print(jumps)
+        
