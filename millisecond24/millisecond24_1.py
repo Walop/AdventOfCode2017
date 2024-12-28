c63cae2c9906a45cc259c790e435188285f9a03b
Joni Lehmuksela <10558717+Walop@users.noreply.github.com>
2024-12-28 12:15:05 +0200

    Just brute force every possible combination

---
+++ millisecond24/millisecond24_1.py
@@ -0,0 +1,36 @@
+import time
+
+def build_all_paths(current, components, path, paths):
+  found = False
+  for c in components:
+    if c in path:
+      continue
+    if c[0] == current:
+      found = True
+      build_all_paths(c[1], components, path + [c], paths)
+    if c[1] == current:
+      found = True
+      build_all_paths(c[0], components, path + [c], paths)
+  if not found:
+    paths.append(path)
+
+with open("input") as file:
+  components = [line.split("/") for line in file.read().splitlines()]
+  for c in components:
+    c[0]=int(c[0])
+    c[1]=int(c[1])
+
+  paths = []
+
+  start = time.perf_counter()
+  build_all_paths(0, components, [],paths)
+  print("Built", len(paths), "paths in", time.perf_counter() - start, "seconds")
+
+  strongest = 0
+
+  for path in paths:
+    strength = sum(sum(component) for component in path)
+    if strength > strongest:
+      strongest = strength
+  
+  print(strongest)
